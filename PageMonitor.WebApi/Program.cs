53a0899dde62646fd0b25c3b31ab5c81928f86ce
Sebastian Szkoła <sszkola@helixsolutions.pl>
2024-04-08 12:29:45 +0200

    Logi -Serilog

--- PageMonitor.WebApi/Program.cs
+++ PageMonitor.WebApi/Program.cs
@@ -1,10 +1,29 @@
+using Serilog;
+
 namespace PageMonitor.WebApi
 {
     public class Program
     {
+        public static string APP_NAME = "PageMonitor.WebApi";
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Aplication", APP_NAME)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
+            builder.Host.UseSerilog((context, services, configuration) => configuration
+                .Enrich.WithProperty("Aplication", APP_NAME)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext());
+
+
 
             // Add services to the container.
 
